c29dd1b7fee12080a9b2112255bfc002341e46e0
wblankenship <william.jblankenship@gmail.com>
2015-12-28 13:53:38 -0500

    Flag parsing offloaded to cli, inventory moved to global state.

--- dante.go
+++ dante.go
@@ -1,40 +1,90 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"github.com/wblankenship/cli"
 	"os"
 )
 
 const version string = "1.1.0"
 
+// Inventory is static and global
+// We will initialize it once and then use it throughout the app
+var inventory Inventory
+
 func main() {
-	// Begin declaring local variables
-	var err error
-	var inventory Inventory
-	var errs []error
-	// End declaring local variables
 
-	// Begin gathering runtime variables
-	var threads = flag.Int("j", 1, "Number of concurrent images to build")
-	var print_version = flag.Bool("v", false, "Print the version of dante")
-	flag.Parse()
-	// End gathering runtime variables
-
-	if *print_version {
-		fmt.Println(version)
-		os.Exit(0)
+	/* Define cli commands and flags */
+	app := cli.NewApp()
+	app.Commands = []cli.Command{
+		{
+			Name:   "test",
+			Usage:  "Build images and run tests defined in inventory.yml",
+			Action: test,
+			Flags: []cli.Flag{
+				cli.IntFlag{
+					Name:  "retries,r",
+					Usage: "Retry on failure",
+					Value: 0,
+				},
+				cli.IntFlag{
+					Name:  "parallel,j",
+					Usage: "Run parallel jobs",
+					Value: 1,
+				},
+			},
+		},
+		{
+			Name:   "push",
+			Usage:  "Push local images to remote registry",
+			Action: push,
+			Flags: []cli.Flag{
+				cli.IntFlag{
+					Name:  "retries,r",
+					Usage: "Retry on failure",
+					Value: 0,
+				},
+				cli.IntFlag{
+					Name:  "parallel,j",
+					Usage: "Run parallel jobs",
+					Value: 1,
+				},
+			},
+		},
 	}
+
+	app.Version = version
+
+	app.Run(os.Args)
+
+}
+
+/*
+populateInventory initializes the global state of the application as directed
+by the user's inventory.yaml file
+*/
+func populateInventory() {
 	// Load the yml definition of images and tests
+	var err error
 	inventory, err = GetInventory()
+
 	if err != nil {
 		// If we can't find the inventory file, there is nothing left for us to do.
 		fmt.Printf("%v\n", err)
 		os.Exit(1)
 	}
+}
 
+func test(c *cli.Context) {
+	populateInventory()
+
+	if c.Int("retries") != 0 {
+		fmt.Printf("# Retries not implemented yet!\n\n")
+	}
+
+	var errs []error
 	// Build the images and run the tests defined in the inventory file
-	errs = runTests(*threads, inventory)
+	errs = runTests(c.Int("parallel"), inventory)
 
 	// Determine if the tests passed or failed
 	if len(errs) > 0 {
@@ -49,4 +99,13 @@ func main() {
 		fmt.Printf("# Conclusion\n\nall tests passed.\n\n", len(errs))
 		os.Exit(0)
 	}
+
+	fmt.Println("Building")
+}
+
+func push(*cli.Context) {
+	populateInventory()
+
+	fmt.Println("# Push not implemented yet")
+	os.Exit(1)
 }
