095633b61b8efa4edf9a57b7acd7214da2f3d184
wblankenship <william.jblankenship@gmail.com>
2016-06-02 17:41:01 -0400

    Dante now copies perms of files to build context

--- fs.go
+++ fs.go
@@ -31,10 +31,9 @@ func copyFile(source string, dest string) (err error) {
 	_, err = io.Copy(destfile, sourcefile)
 	if err == nil {
 		sourceinfo, err := os.Stat(source)
-		if err != nil {
+		if err == nil {
 			err = os.Chmod(dest, sourceinfo.Mode())
 		}
-
 	}
 
 	return
